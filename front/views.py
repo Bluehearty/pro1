f31ec4bba119c6a413692e6ef71dcd54b2660c41
Bluehearty <1195074058@qq.com>
2019-04-28 19:46:20 +0800

    add session

--- front/views.py
+++ front/views.py
@@ -3,11 +3,14 @@ from django.http import HttpResponse
 from . import models
 from .forms import UserForm,RegisterForm,UserInfoForm
 
-login_state = 0 #0未登陆，1登陆成功
-mobile = '123456789'
+mobileMMMMMMM = '123456789'
 
 def login(request):
-    if request.method =='POST':
+    #如果is_login是True则不需要登陆
+    if request.session.get('is_login', None):
+        return redirect('index')
+
+    if request.method == 'POST':
         '''mobile= request.POST.get('mobile',None)
         password = request.POST.get('password',None)
         message = "请检查填写的内容！"
@@ -31,15 +34,14 @@ def login(request):
         login_form = UserForm(request.POST)
         message = "请检查填写的内容！"
         if login_form.is_valid():
-            global mobile
-            mobile = login_form.cleaned_data['mobile']
-            password = login_form.cleaned_data['password']
             try:
-                user = models.User.objects.get(mobile=mobile)
-                if user.password == password:
-                    global login_state
-                    login_state = 1
-                    return redirect('index')
+                user = models.User.objects.get(mobile=login_form.cleaned_data['mobile'])
+                if user.password == user.password:
+                    request.session['is_login'] = True
+                    request.session['user_mobile'] = user.mobile
+                    request.session['user_name'] = user.name
+                    request.session['password'] = user.password
+                    return redirect('/index/')
                 else:
                     message = "密码不正确！"
             except:
@@ -52,19 +54,22 @@ def login(request):
         return HttpResponse('method既不是POST也不是GET!')
 
 def index(request):
-    if login_state == 0:
-        return redirect('login')
-    elif login_state == 1:
-        return render(request,'index.html')
+    if 'is_login' in request.session:
+        if request.session['is_login']:
+            return render(request,'index.html')
+        else:
+            return redirect('login')
     else:
-        return HttpResponse('错误！')
-
+        request.session['is_login'] = False
+        return redirect('login')
 
 def register(request):
     if request.method == "POST":
+        print('1')
         register_form = RegisterForm(request.POST)
         message = '请检查填写的内容！'
         if register_form.is_valid(): #验证
+            print('12')
             mobile = register_form.cleaned_data['mobile']
             password1 = register_form.cleaned_data['password1']
             password2 = register_form.cleaned_data['password2']
@@ -90,6 +95,7 @@ def register(request):
                     message = '邮箱已被注册，请更换邮箱地址！'
                     #return redirect('/register/')
                     return render(request, 'register.html', locals())
+                print('3')
                 # 当一切都OK时，创建新用户
                 new_user = models.User.objects.create()
                 new_user.mobile = mobile
@@ -105,6 +111,9 @@ def register(request):
                 new_user.save()
                 return redirect('/login/') #自动跳转到登陆页面
 
+        else:
+            return HttpResponse('dw')
+
     elif request.method == 'GET':
         register_form = RegisterForm()
         return render(request, 'register.html', locals())
@@ -114,9 +123,10 @@ def register(request):
 
 
 def logout(request):
-    global login_state
-    login_state = 0
-    return redirect('/login/')
+    if not request.session.get('is_login',None):
+        return redirect('/login/')
+    request.session.flush()
+    return redirect('index')
 
 def base1(request):
     return render(request,'base1.html')
@@ -126,15 +136,13 @@ def applied(request):
     return render(request,'applied.html')
 
 def user(request):
-    global mobile
     #current_mobile = models.User.objects.filter(mobile=mobile)
-    current_mobile = models.User.objects.get(mobile=mobile)
+    current_mobile = models.User.objects.get(mobile=request.session['user_mobile'])
 
     return render(request, 'user.html',{'current_mobile':current_mobile})
 
 def info(request):
-    global mobile
-    current_mobile = models.User.objects.get(mobile=mobile)
+    current_mobile = models.User.objects.get(mobile=request.session['user_mobile'])
     if request.method == 'POST':
         info_form = UserInfoForm(request.POST)
         if info_form.is_valid():
@@ -158,8 +166,7 @@ def info(request):
 
 
 def setpassword(request):
-    global mobile
-    current_mobile = models.User.objects.get(mobile=mobile)
+    current_mobile = models.User.objects.get(mobile=request.session['user_mobile'])
     if request.method == 'POST':
         pwd = request.POST.get('pwd',None)
         new_pwd = request.POST.get('new_pwd',None)
